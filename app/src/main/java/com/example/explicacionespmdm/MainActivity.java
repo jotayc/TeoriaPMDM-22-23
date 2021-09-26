2fc1a579e44974d068db65ad64c0fc88d81a6a78
JC <jcalfaroprof@gmail.com>
2021-09-26 19:31:42 +0200

    Actividad 1. Añadido métodos del ciclo de vida de las actividades

--- app/src/main/java/com/example/explicacionespmdm/MainActivity.java
+++ app/src/main/java/com/example/explicacionespmdm/MainActivity.java
@@ -51,4 +51,37 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    //Todo 6. Los siguientes métodos forman parte del ciclo de vida de cualquier actividad
+    //Todo    Se podría ver como un diagrama de estados.
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
