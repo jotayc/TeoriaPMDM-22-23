00c0325bdabd6b9723de191b54907b196a31013b
JC <jcalfaroprof@gmail.com>
2021-10-02 20:44:01 +0200

    Finalizada explicación ActivityForResult

--- app/src/main/java/com/example/explicacionespmdm/MainActivity.java
+++ app/src/main/java/com/example/explicacionespmdm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.explicacionespmdm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Actividad Modificación de comunicación entre actividades B hacia A
@@ -20,16 +23,18 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    EditText edTxtMain;
+    TextView txtVwMsg;
     Button btnMain;
 
+    //Todo 1.2 el número es indiferente pero no debe repetirse para otros request code
+    public static int RQ_SECOND_ACT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edTxtMain = (EditText) findViewById(R.id.edtxt_main);
+        txtVwMsg = (TextView) findViewById(R.id.txtVw_main);
         btnMain = (Button) findViewById(R.id.btn_main);
 
         btnMain.setOnClickListener(new View.OnClickListener() {
@@ -37,14 +42,40 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,SecondActivity.class);
 
-                String msg = edTxtMain.getText().toString();
-                if(msg != null && msg != "")
-                    i.putExtra("INFO", msg);
+                //Todo 1. Llamamos la variante del método que lanza la actividad que indica que esta
+                //Todo -> esta esperará un resultado.
 
-                startActivity(i);
+                // Todo 1.1 El segundo parametro "Request Code" se encargará de identificar de qué actividad
+                // Todo -> está recibiendo el mensaje.
+
+                startActivityForResult(i,RQ_SECOND_ACT);
             }
         });
 
+
+    }
+
+    //Todo 3.1 Sobrecargamos el método onActivityResult
+    /**
+     * Método que recibe información de una actividad lanzada posteriormente
+     * @param requestCode Codigo que identifica qué actividad envía el mensaje.
+     * @param resultCode Código que identifica si el mensaje que ha recibido es correcto o no.
+     * @param data Contiene el mensaje.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Todo 3.2 Usamos RQ_SECOND_ACT para saber si el mensaje proviene de la segunda actividad
+        if(requestCode == RQ_SECOND_ACT){
+            //Todo 3.3 Usamos RESULT_OK para saber si el mensaje que llega es el correcto.
+            if(resultCode==RESULT_OK){
+                //Todo 3.4 Extraemos la información del intent y lo usamos
+                String msg = data.getStringExtra("result");
+                txtVwMsg.setText(msg);
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/example/explicacionespmdm/SecondActivity.java
+++ app/src/main/java/com/example/explicacionespmdm/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.explicacionespmdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,20 +24,36 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        txtVwSec    = (TextView) findViewById(R.id.txtVw_scnd);
+        txtVwSec    = (TextView) findViewById(R.id.txtVw_scnd_tit);
         btnSec      = (Button) findViewById(R.id.btn_fin_scnd);
         editText    = (EditText) findViewById(R.id.edTxt_scnd);
 
 
-        Intent i = getIntent();
-
-        String msg = i.getStringExtra("INFO");
-        txtVwSec.setText(msg);
 
 
         btnSec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String msg = editText.getText().toString();
+                if(msg != null && !msg.equals("")){
+                    //Todo 2. En este caso creamos un intent para que devuelva el mensaje.
+                    //Todo -> Este se entregará cuando la instancia de la actividad que lo devuelve finalice.
+                    Intent returnIntent = new Intent();
+
+                    //Todo 2.1 Se añade el mensaje al intent.
+                    returnIntent.putExtra("result",msg);
+
+                    //Todo 2.2 Se llama al metodo que añade el intent al resultado.
+                    //Todo -> En este caso admite un parámetro inicial "ResultCode" que podrá ser
+                    //Todo -> dos valores estáticos dentro de la clase Activity:
+                    // Todo -> RESULT_OK en caso de que el resultado devuelto sea correcto
+                    //Todo ->  RESULT_CANCELED  en caso de que el resultado devuelto sea incorrecto.
+                    setResult(Activity.RESULT_OK,returnIntent);
+
+                    //Todo 2.3 Finalizamos la actividad para devolver el resultado.
+                    finish();
+                }
+
 
             }
         });
