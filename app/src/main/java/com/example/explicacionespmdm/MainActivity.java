9dec7a6b2baf69236d93d3cebd56502760f283fa
JC <jcalfaroprof@gmail.com>
2021-10-02 19:47:30 +0200

    Adaptada vista para paso de mensajes entre MainActivity y SecondActivity

--- app/src/main/java/com/example/explicacionespmdm/MainActivity.java
+++ app/src/main/java/com/example/explicacionespmdm/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -21,22 +22,27 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtVwMain;
+    EditText edTxtMain;
     Button btnMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtVwMain = (TextView) findViewById(R.id.txtVw_main);
+        edTxtMain = (EditText) findViewById(R.id.edtxt_main);
         btnMain = (Button) findViewById(R.id.btn_main);
 
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,SecondActivity.class);
-                i.putExtra("INFO", "Hola desde el main");
+
+                String msg = edTxtMain.getText().toString();
+                if(msg != null && msg != "")
+                    i.putExtra("INFO", msg);
+
                 startActivity(i);
             }
         });
