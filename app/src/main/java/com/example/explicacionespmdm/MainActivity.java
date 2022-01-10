6f0734543b8b65ac5ce90a690432f743b9b651e7
JC <jcalfaroprof@gmail.com>
2022-01-10 14:16:04 +0100

    Completado reproductor de música con MediaPlayer

--- app/src/main/java/com/example/explicacionespmdm/MainActivity.java
+++ app/src/main/java/com/example/explicacionespmdm/MainActivity.java
@@ -2,28 +2,111 @@ package com.example.explicacionespmdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
- * PROYECTO BASE
+ * MEDIA PLAYER
  * @author JC
  * @version 0.1
  *
- *  La idea del proyecto es concentrar en uno solo todas las tareas explicativas
- *  del módulo de 2º de DAM, Programación Multimedia y dispositivos móviles,
- *  haciendo uso de ramas a través de git.
+ *  Ejercicio que implementa un reproductor de música básico. El proyecto está incompleto, ya que
+ *  debe añadirse más elementos que controlen el flujo de de los estados de los sonidos.
  *
- *  Cada rama será un proyecto de un apartado explicativo independiente.
+ *  Consideraciones previas:
  *
- *  Esta rama SE DEBE DEJAR SIN IMPLEMENTAR para tenerla como base para comenzar ramas de futuras
- *  explicaciones.
+ *  1. Consultar la documentación oficial.
+ *  2. IMPORTANTE: Comprender y consultar el diagrama de estado del objeto MediaPlayer:
+ *  https://developer.android.com/reference/android/media/MediaPlayer#StateDiagram
  *
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txtInfo;
+    private Button btnPlay;
+    private Button btnStop;
+
+    //Todo 1. Creamos el objeto MediaPlayer. Dependiendo de su estado se podrá llamara a diferentes
+    // métodos
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtInfo = (TextView) findViewById(R.id.txt_info);
+        btnPlay = (Button) findViewById(R.id.btn_play);
+        btnStop = (Button) findViewById(R.id.btn_stop);
+
+        btnPlay.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.btn_play:
+                //Todo 2. Inicializamos el objeto. Aquí hay que tener en cuenta que se puede
+                // inicializar de dos formas:
+                // Forma 1. Como un objeto normal a través de 'new'. De esta forma el objeto pasará
+                //          al estado 'idle' o inactivo.
+                // Forma 2. A través del método estático 'create'. De esta forma el objeto pasará
+                // directamente al estado 'prepare' o preparado
+                if(mediaPlayer == null){
+                    mediaPlayer = MediaPlayer.create(this, R.raw.forelisa);
+                }
+
+                //Todo 3. Listener que se encargará de llamarse cuando la música llegue al final.
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        stopMusic();
+                    }
+                });
+
+                //Todo 4. Comenzamos la reproducción
+                mediaPlayer.start();
+                txtInfo.setText("Reproduciendo...");
+
+                //Todo 5. Se controla la visibilidad de los botones para controlar que el usuario
+                // no active funciones que el estado del objeto no lo permita
+                btnPlay.setEnabled(false);
+                btnStop.setEnabled(true);
+                break;
+
+            case R.id.btn_stop:
+                if(mediaPlayer != null){
+                    //Todo 6. Paramos la música.
+                    mediaPlayer.stop();
+                    stopMusic();
+                    btnStop.setEnabled(false);
+                    btnPlay.setEnabled(true);
+                    txtInfo.setText("Música preparada");
+                }
+
+                break;
+        }
+    }
+
+    private void stopMusic() {
+        // Todo 7. Se libera la memoria y se elimina la instancia.
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Todo 8. Se controla que si el usuario cambia de actividad o para la aplicación
+        // la música pare
+        stopMusic();
     }
 }
